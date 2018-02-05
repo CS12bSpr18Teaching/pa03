4737be2ec60e1bc3a8dcb0f1e23ff7456dee9d08
Timothy J Hickey <tjhickey@brandeis.edu>
2018-02-05 09:37:01 -0500

    added TimCircle to DrawingModel init

--- TimCircle.java
+++ TimCircle.java
@@ -1,6 +1,13 @@
 package pa03;
 
+import java.awt.Color;
+
 public class TimCircle extends CircleShape{
+
+  public TimCircle(){
+    super();
+    this.color = new Color(0,0,0);
+  }
 
   public static void main(String[] args){
     TimCircle x = new TimCircle();
